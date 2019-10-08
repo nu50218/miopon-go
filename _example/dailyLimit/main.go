40aa2e4bed33438283555cdfa4636093828cdb25
nu50218 <nu_ll@icloud.com>
2019-10-08 19:18:26 +0900

    update main.go

--- _example/dailyLimit/main.go
+++ _example/dailyLimit/main.go
@@ -6,14 +6,19 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"net/http"
+	"time"
+
+	"github.com/nu50218/miopon-go/coupon"
 
 	"github.com/nu50218/miopon-go"
 )
 
 type Settings struct {
-	DeveloperID    string   `json:"developer_id"`
-	AccessTokens   []string `json:"access_tokens"`
-	IntervalMinute int64    `json:"interval_minute"`
+	DeveloperID    string `json:"developer_id"`
+	AccessToken    string `json:"access_tokens"`
+	IntervalSecond int    `json:"interval_second"`
+	VolumeLimit    int    `json:"volume_limit"`
 }
 
 const settingsFilename = "settings.json"
@@ -39,8 +44,8 @@ func main() {
 
 		settings := &Settings{
 			DeveloperID:    developerID,
-			AccessTokens:   []string{},
-			IntervalMinute: 1,
+			AccessToken:    "",
+			IntervalSecond: 60,
 		}
 		b, err := json.MarshalIndent(settings, "", "  ")
 		if err != nil {
@@ -56,4 +61,105 @@ func main() {
 		return
 	}
 
+	b, err := ioutil.ReadFile(settingsFilename)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	settings := &Settings{}
+	if err := json.Unmarshal(b, settings); err != nil {
+		log.Fatalln(err)
+	}
+
+	client := miopon.New(settings.DeveloperID, &miopon.Settings{
+		GetCouponInterval: 12 * time.Second,
+		PutCouponInterval: 60 * time.Second,
+		GetPacketInterval: 12 * time.Second,
+	})
+
+	for {
+		check(settings.AccessToken, client, settings.VolumeLimit)
+		time.Sleep(time.Duration(settings.IntervalSecond) * time.Second)
+	}
+
+}
+
+func check(accessToken string, client *miopon.Client, volumeLimit int) {
+	body, statusCode, err := client.GetPacket(accessToken)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	if statusCode != http.StatusOK {
+		log.Println(statusCode, body.ReturnCode)
+		return
+	}
+
+	couponInfo := []*coupon.CouponInfo{}
+	for _, packetLogInfo := range body.PacketLogInfo {
+		c := coupon.CouponInfo{}
+		for _, hdoInfo := range packetLogInfo.HdoInfo {
+			if len(hdoInfo.PacketLog) == 0 {
+				continue
+			}
+			if hdoInfo.PacketLog[len(hdoInfo.PacketLog)-1].WithCoupon > volumeLimit {
+				c.HdoInfo = append(c.HdoInfo, &coupon.HdoInfo{
+					HdoServiceCode: hdoInfo.HdoServiceCode,
+					CouponUse:      true,
+				})
+			}
+		}
+
+		for _, hduInfo := range packetLogInfo.HduInfo {
+			if len(hduInfo.PacketLog) == 0 {
+				continue
+			}
+			if hduInfo.PacketLog[len(hduInfo.PacketLog)-1].WithCoupon > volumeLimit {
+				c.HduInfo = append(c.HduInfo, &coupon.HduInfo{
+					HduServiceCode: hduInfo.HduServiceCode,
+					CouponUse:      true,
+				})
+			}
+		}
+
+		for _, hdxInfo := range packetLogInfo.HdxInfo {
+			if len(hdxInfo.PacketLog) == 0 {
+				continue
+			}
+			if hdxInfo.PacketLog[len(hdxInfo.PacketLog)-1].WithCoupon > volumeLimit {
+				c.HdxInfo = append(c.HdxInfo, &coupon.HdxInfo{
+					HdxServiceCode: hdxInfo.HdxServiceCode,
+					CouponUse:      true,
+				})
+			}
+		}
+
+		if len(c.HdoInfo) != 0 || len(c.HduInfo) != 0 || len(c.HdxInfo) != 0 {
+			couponInfo = append(couponInfo, &c)
+		}
+	}
+
+	if len(couponInfo) != 0 {
+		body, statusCode, err := client.PutCoupon(accessToken, couponInfo)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		if statusCode != http.StatusOK {
+			log.Println(statusCode, body.ReturnCode)
+			return
+		}
+		log.Println("switched off:")
+		for _, c := range couponInfo {
+			for _, hdoInfo := range c.HdoInfo {
+				log.Printf("\t%s", hdoInfo.HdoServiceCode)
+			}
+			for _, hduInfo := range c.HduInfo {
+				log.Printf("\t%s", hduInfo.HduServiceCode)
+			}
+			for _, hdxInfo := range c.HdxInfo {
+				log.Printf("\t%s", hdxInfo.HdxServiceCode)
+			}
+		}
+	}
 }
